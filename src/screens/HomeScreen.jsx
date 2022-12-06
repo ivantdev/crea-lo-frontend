6381cfa8c1e41e86ce7c547d354f02358453405e
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-12-05 20:53:07 -0500

    outline of cylinder structure

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react'
+import "../css/HomeScreen.css"
 import { getTagsByCategory } from "../api"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const HomeScreen = () => {
   const [pasoTags, setPasoTags] = React.useState([])
@@ -19,9 +22,19 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div id="canvas-container">
+      <Canvas className='canvas'>
+        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh >
+          <cylinderGeometry args={[5, 5, 5, 32, 6
+          ]} />
+          <meshStandardMaterial wireframe transparent />
+        </mesh>
+      </Canvas>
+    </div>
   )
-  u
 }
 
 export default HomeScreen
