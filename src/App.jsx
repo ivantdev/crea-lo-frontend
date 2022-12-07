6c47aa7e7aae5d25e6f6d2596e584b0c217228ce
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-12-07 15:47:28 -0500

    Rearranging things

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { HomeScreen, TagScreen, EditorialScreen, NotFound } from "./screens"
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/editorial" element={<EditorialScreen />} />
-        <Route path="/tags/:tag" element={<TagScreen />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter >
+    <Routes location={location}>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/editorial" element={<EditorialScreen />} />
+      <Route path="/tags/:tag" element={<TagScreen />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
