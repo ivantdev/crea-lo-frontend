5461e976dee4bb6193c7e0ae8b3ce616676f76cd
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-12-12 21:12:15 -0500

    Api calls to get tag content created

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,30 @@ const getTagsByCategory = async (category) => {
     return data
 }
 
+const getTagImages = async (tag) => {
+    const data = await axios.get(`${BASE_URL}/api/tags?populate[images][populate][0]=link&filters[name][$eq]=${tag}`)
+    return data
+}
+
+const getTagVideos = async (tag) => {
+    const data = await axios.get(`${BASE_URL}/api/tags?populate[0]=videos&filters[name][$eq]=${tag}`)
+    return data
+}
+
+const getTagCreations = async (tag) => {
+    const data = await axios.get(`${BASE_URL}/api/tags?populate[0]=creations&filters[name][$eq]=${tag}`)
+    return data
+}
+
 const getTagContent = async (tag) => {
-    const data = await axios.get(`${BASE_URL}/api/tags?populate=%2A&filters[name][$eq]=${tag}`)
+    const images = await getTagImages(tag)
+    const videos = await getTagVideos(tag)
+    const creations = await getTagCreations(tag)
+    return { images, videos, creations }
+}
+
+const getConcepts = async () => {
+    const data = await axios.get(`${BASE_URL}/api/concepts`)
     return data
 }
 
--- src/screens/TagScreen.jsx
+++ src/screens/TagScreen.jsx
@@ -2,26 +2,63 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getTagContent } from '../api'
 import { useNavigate } from 'react-router-dom'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+import Image from 'mui-image'
 
 const TagScreen = () => {
     const tag = useParams().tag
-    const [tagData, setTagData] = useState(null)
+
+    const [creation, setCreation] = useState(null)
+    const [image, setImage] = useState(null)
+    const [video, setVideo] = useState(null)
+
     const navigate = useNavigate()
 
     useEffect(() => {
-        getTagContent(tag).then((data) => {
-            if (data.data.data.length > 0) {
-                setTagData(data.data.data[0])
-                console.log(data.data.data)
-            } else {
-                // redirect to 404
-                navigate('/404')
-            }
-        })
+        const fetchData = async () => {
+            const data = await getTagContent(tag)
+            const creationsData = data.creations.data.data[0].attributes.creations.data
+            const creationsLength = creationsData.length
+            const imagesData = data.images.data.data[0].attributes.images.data
+            const imagesLength = imagesData.length
+            const videosData = data.videos.data.data[0].attributes.videos.data
+            const videosLength = videosData.length
+            // choose random creation
+            setCreation(creationsData[Math.floor(Math.random() * creationsLength)])
+            // choose random image
+            setImage(imagesData[Math.floor(Math.random() * imagesLength)])
+            // choose random video
+            setVideo(videosData[Math.floor(Math.random() * videosLength)])
+
+        }
+        fetchData()
     }, [tag])
 
+
+    // Don't touch this
     return (
-        <div>TagScreen</div>
+        image ? creation ? video ? (
+            <Carousel>
+                {/* {creation && (
+                <div>
+                    <img src={creation.attributes.image_url} />
+                    <p className="legend">{creation.attributes.title}</p>
+                </div>
+            )} */}
+                {image && (
+                    <div>
+                        <Image src={image ? image.attributes.link.data.attributes.formats.large.url : ''} />
+                        <p className="legend">{image.attributes.name}</p>
+                    </div>
+                )}
+                {video && (
+                    <div>
+                        <iframe width="560" height="315" src={video.attributes.link} title={video.attributes.name} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <p className="legend">{video ? video.attributes.name : ''}</p>
+                    </div>
+                )}
+            </Carousel>) : null : null : null
     )
 }
 
