63aa955d1c5f64b057bbaf402706bec71acb6c02
Ivan Castellanos <ivan@ivant.dev>
2023-06-03 00:10:43 -0500

    home description

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -11,61 +11,49 @@ import home_5 from "../assets/images/Home 5.jpg"
 
 
 const HomeScreen = () => {
-    const { homeIndexPalette, theme } = React.useContext(ColorModeContext)
-    const location = useLocation()
-    const background = [
-        [
-            // BG 1
-            {
-                backgroundImage: `url('${home_1}')`,
+  const { homeIndexPalette, theme } = React.useContext(ColorModeContext)
+  const location = useLocation()
+  const background = [
+      // BG 1
+      {
+        backgroundImage: `url('${home_1}')`,
+      },
+      // BG 2
+      {
+        backgroundImage: `url('${home_2}')`,
+      },
+      // BG 3
+      {
+        backgroundImage: `url('${home_3}')`,
+      },
+      // BG 4
+      {
+        backgroundImage: `url('${home_4}')`,
+      },
+      // BG 5
+      {
+        backgroundImage: `url('${home_5}')`,
+      },
+  ]
 
-            },
-        ],
-        [
-            // BG 2
-            {
-                backgroundImage: `url('${home_2}')`,
-
-            },
-        ],
-        [
-            // BG 3
-            {
-                backgroundImage: `url('${home_3}')`,
-
-            },
-        ],
-        [
-             // BG 4
-             {
-                backgroundImage: `url('${home_4}')`,
-
-            },
-        ],
-        [
-             // BG 5
-             {
-                backgroundImage: `url('${home_5}')`,
-
-            },
-        ],
-    ]
-
-    return (
-        <>
-            {
-                background[homeIndexPalette].map((background, index) => (
-                    <div key={index} id="home-background" style={background} />
-                ))
-            }
-            <div id="home-logo-container">
-                <img src={logo} alt="logo" id="home-logo" />
-                <div id="home-logo-text">
-                    {/* <h1>Otros mundos posibles en la UNAL</h1> */}
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div id="home-background" style={background[homeIndexPalette]} />
+      <div id="home-logo-container">
+        <img src={logo} alt="logo" id="home-logo" />
+        <div className='home-logo-text mt-5'>
+          <p className='parrafo center underline' style={{ mixBlendMode: theme.palette.background.mixBlendMode, "--underline-color": theme.palette.background.default, "--underline-text-color": theme.palette.text.primary }}>
+            Crea-lo es un proyecto del Área de Cultura de Bienestar Universitario UNAL-Sede Bogotá, que fomenta la reflexión y creación de múltiples prácticas desde lo interdisciplinar y lo micropolítico, promoviendo el hacer cultural desde lo sensible, las emociones y las afectividades e invitando a la emergencia de lo recíproco, la corresponsabilidad y la no instrumentalización de la cultura a través de miradas interdisciplinarias.
+          </p>
+        </div>
+        <div className='home-logo-text mt-2'>
+          <p className="parrafo center underline" style={{ mixBlendMode: theme.palette.background.mixBlendMode, "--underline-color": theme.palette.background.default, "--underline-text-color": theme.palette.text.primary }}>
+            A continuación encontrarás trazos de memorias de los diferentes momentos del proyecto Crea-lo, desde el 2020 a 2023. Te invitamos a viajar en este universo virtual que se construyó con materiales diversos gestados con los participantes en los laboratorios sensibles.
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default HomeScreen
